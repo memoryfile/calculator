01eb2dc49235237517c480c93a9372bb01ec33c7
memoryfile <cooperlewis@protonmail.com>
2022-07-22 16:52:21 -0400

    Add math functions, buttons, header

---
+++ main.js
@@ -0,0 +1,30 @@
+// let input = a;
+// let input2 = b;
+let z = 0;
+const inputtedNumbers = [x, y];
+
+function add(x, y) {
+  let sum = x + y
+  return sum;
+}
+
+function subtract(x, y) {
+  let difference = x - y
+  return difference;
+}
+
+function multiply(x, y) {
+  let product = x * y
+  return product;
+}
+
+function divide(x, y) {
+  let quotient = x / y
+  return quotient;
+}
+
+function createCalculatorButtons() {
+  while (z > 10) {
+    z += 1;
+  }
+}
